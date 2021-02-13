62d60f4bf00d63e2ede713601d332b1226a2c414
dapomakanjuola <Dm262@njit.edu>
2021-02-12 22:27:42 -0500

    * HelloPlanet.sln:
    * Program.cs: commit: changed to my name feature: added readme

--- HelloPlanet/Program.cs
+++ HelloPlanet/Program.cs
@@ -6,7 +6,11 @@ namespace HelloPlanet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Planet!");
+            Console.WriteLine("Hello Dapo!");
+
+            string fname = "Dapomola";
+            string lname = "Makanjuola";
+            Console.WriteLine("My name is {0} {1}", fname ,lname);
         }
     }
 }

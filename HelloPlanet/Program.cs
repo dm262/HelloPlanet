d5d6bdbbed2a653a512787be5dca76ac7a8e8527
dapomakanjuola <Dm262@njit.edu>
2021-02-12 21:41:37 -0500

    first commit * HelloPlanet.sln: * Program.cs: * HelloPlanet.csproj:
    * .gitignore: First commit

---
+++ HelloPlanet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloPlanet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Planet!");
+        }
+    }
+}
